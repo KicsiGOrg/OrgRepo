8ebbda642b000340e5457dd4fc8a7983a11bd715
Balogh Gergely <gergely.balogh@icellmobilsoft.hu>
2018-04-19 16:56:32 +0200

    Try catch beépítve a TimeChanger-be

--- TimeChanger/src/SecondToTime.java
+++ TimeChanger/src/SecondToTime.java
@@ -2,9 +2,8 @@ import java.util.Scanner;
 
 public class SecondToTime {
 
-	static Scanner sc = new Scanner(System.in);
 	static int second;
-	
+
 	static void time(int mp) {
 		int second = mp;
 		int hour = second / 3600;
@@ -15,19 +14,26 @@ public class SecondToTime {
 	}
 
 	public static void main(String[] args) {
-		
-		getSecond();
+		Scanner sc = new Scanner(System.in);
+		getSecond(sc);
 		sc.close();
 		time(second);
 	}
-	private static void getSecond() {
+
+	private static void getSecond(Scanner sc) {
+
 		do {
-		System.out.print("Kérem adja meg a vizsgálandó idő másodpercben: ");
-		second = sc.nextInt();
-		sc.nextLine();
-			if(second <= 0) {
-				System.out.println("A megadott szám túl kicsi, 0-nál nagyobb számot adjon meg.");
+			try {
+				System.out.print("Kérem adja meg a vizsgálandó időt másodpercben: ");
+				second = sc.nextInt();
+				sc.nextLine();
+				if (second <= 0) {
+					System.out.println("A megadott szám túl kicsi, 0-nál nagyobb számot adjon meg.");
+				}
+			} catch (Exception e) {
+				sc.nextLine();
+				System.out.println("Nana Balázs!\nNem garázdálkodunk a programban oda nem illő dolgokkal :)\n");
 			}
-		}while(second <= 0);
+		} while (second <= 0);
 	}
 }
