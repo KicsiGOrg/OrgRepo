eda4a897406cf4210a1c8f30afdce127e4893841
Unknown <balogh.gergely83@gmail.com>
2018-04-29 06:04:46 +0200

    Censor + Hangman

--- Hangman/src/HangmanTwo.java
+++ Hangman/src/HangmanTwo.java
@@ -1,34 +1,42 @@
 import java.util.Random;
 import java.util.Scanner;
 
-/*
- * Hangman
- * Feladványok - KÉSZ
- * Feladvány random kiválasztása, visszaadása uppercase-elve - KÉSZ
- * Feladvány karakterekre bontása és tárolása '_' és ' ' karakterekben - KÉSZ (char[]-ben tároljuk le! )
- * Feladványok megjelnítése - KÉSZ (char[]-t adunk át nincs visszatérési érték, csak print, ami foreach-el végig iterál a betűkön és azokat kiírja egy sorba karakterenként + ' ', majd a végén egy plusz sortörés!)
- * Input bekérés
- * Input ellenőrzése a feladványban, amennyiben talál a hozzátartozó '_' jelet írja felül a betűvel
- * Hiba esetén írja ki a hibás betűt, ami nem szerepel a feladványban
- * 10 hibázási / idő előtti megfejtésre vizsgálat
- */
-
 public class HangmanTwo {
 
+	static Scanner sc = new Scanner(System.in);
+	
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
 		String riddle = getRiddle();
-		System.out.println(riddle); // a program végén törölni
 		char[] maskedRiddle = maskedSolution(riddle);
 		char[] wrongLetters = new char[10];
 		int mistakes = 0;
-		boolean solution = false;
 		do {
 			printMaskedRiddle(maskedRiddle);
-			String input = sc.nextLine();
+			char letter = getInput();
+			boolean y = x(riddle, maskedRiddle, letter);
+			if(!y) {
+				wrongLetters[mistakes] = letter;
+				mistakes++;
+			}
+			printWrongLetters(wrongLetters, mistakes);
 		}
-		while(mistakes != 10 || solution != true);
+		while(mistakes < 10 && !isSolved(maskedRiddle));
+		printMaskedRiddle(maskedRiddle);
+		System.out.println("Vége a játéknak, " + (isSolved(maskedRiddle) ? "Ön nyert!" : "Ön vesztett!"));
+		if (!isSolved(maskedRiddle)) {
+			System.out.println("A feladvány: " + riddle);
+		}
 		sc.close();
+	}
+
+	private static void printWrongLetters(char[] wrongLetters, int mistakes) {
+		System.out.print("A következő " + mistakes + " betűk nincsenek benne a megfejtésben: ");
+		String separator = "";
+		for (int i = 0; i < mistakes; i++) {
+			System.out.print(separator + wrongLetters[i]);
+			separator = ", ";
+		}
+		System.out.println();
 	}
 
 	private static char[] maskedSolution(String riddle) {
@@ -43,6 +51,15 @@ public class HangmanTwo {
 		return mask;
 	}
 
+	private static boolean isSolved(char[] solution) {
+		for (int i = 0; i < solution.length; i++) {
+			if (solution[i] == '_') {
+				return false;
+			}
+		}
+		return true;
+	}
+	
 	private static String getRiddle() {
 		String[] riddleArray = { "Almafa", "Körtefa", "Szilvafa", "Akasztófa", "Bitófa", "Ajtófélfa" };
 		Random random = new Random();
@@ -57,4 +74,22 @@ public class HangmanTwo {
 		System.out.println();
 	}
 	
+	private static char getInput () {
+		System.out.print("Kérem adjon meg egy betűt: ");
+		String input = sc.nextLine();
+		String inputUpperCase = input.toUpperCase();
+		char firstLetter = inputUpperCase.charAt(0);
+		return firstLetter;
+	}
+
+	private static boolean x(String riddle, char[] maskedRiddle, char letter) {
+		boolean xy = false;
+		for(int i = 0; i < riddle.length(); i++) {
+			if(riddle.charAt(i) == letter) {
+				maskedRiddle[i] = letter;
+				xy = true;
+			}
+		}
+		return xy;
+	}
 }
