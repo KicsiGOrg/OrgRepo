a838ec84fbba8e769d321d9adb2d7c0174fc7e99
Balogh Gergely <gergely.balogh@icellmobilsoft.hu>
2018-05-02 08:34:32 +0200

    Auto stash before merge of "master" and "origin/master"

--- Hangman/src/HangmanThree.java
+++ Hangman/src/HangmanThree.java
@@ -1,17 +1,8 @@
 import java.util.Random;
 import java.util.Scanner;
 
-/**
- * Implementation of the Hangman game.
- */
-public class Hangman {
+public class HangmanThree {
 
-	/**
-	 * Program entry point.
-	 *
-	 * @param args
-	 *            Command line arguments.
-	 */
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		String riddleWord = generateRiddle();
@@ -29,22 +20,13 @@ public class Hangman {
 			printWrongLetters(wrongGuesses, numberOfWrongGuesses);
 		} while (numberOfWrongGuesses < 10 && !isSolved(solution));
 		printRiddle(solution);
-		System.out.println("\nVége a játéknak, " + (isSolved(solution) ? "Ön nyert!" : "Ön vesztett!"));
+		System.out.println("Vége a játéknak, " + (isSolved(solution) ? "Ön nyert!" : "Ön vesztett!"));
 		if(!isSolved(solution)) {
-			System.out.println("\n" + riddleWord);
+			System.out.println("\nA megfejtés: " + riddleWord);
 		}
 		scanner.close();
 	}
 
-	/**
-	 * This method prints out the wrong letters guessed so far to the standard
-	 * output.
-	 *
-	 * @param wrongGuesses
-	 *            The wrong guesses to print.
-	 * @param numberOfWrongGuesses
-	 *            The number of wrong guesses.
-	 */
 	private static void printWrongLetters(char[] wrongGuesses, int numberOfWrongGuesses) {
 		System.out.format("Nincs benne ez a(z) %d betű: ", numberOfWrongGuesses);
 		String separator = "";
@@ -55,14 +37,6 @@ public class Hangman {
 		System.out.println();
 	}
 
-	/**
-	 * Checked if the solution is complete.
-	 *
-	 * @param solution
-	 *            The solution to check.
-	 * @return True if the solution does not contain any underscore characters,
-	 *         false otherwise.
-	 */
 	private static boolean isSolved(char[] solution) {
 		for (int i = 0; i < solution.length; i++) {
 			if (solution[i] == '_') {
@@ -72,17 +46,6 @@ public class Hangman {
 		return true;
 	}
 
-	/**
-	 * Update the solution by looking at the currently guessed letter.
-	 *
-	 * @param riddleWord
-	 *            The word we need to guess.
-	 * @param solution
-	 *            The solution so far.
-	 * @param guessedLetter
-	 *            The letter the user guessed.
-	 * @return True if the letter was found in the riddle word, false otherwise.
-	 */
 	private static boolean updateSolution(String riddleWord, char[] solution, char guessedLetter) {
 		boolean riddleWordContainsTheGuessedLetter = false;
 		for (int i = 0; i < riddleWord.length(); i++) {
@@ -94,30 +57,13 @@ public class Hangman {
 		return riddleWordContainsTheGuessedLetter;
 	}
 
-	/**
-	 * Reads the next letter from the user.
-	 *
-	 * @param scanner
-	 *            The scanner to use for the reading.
-	 * @param askMessage
-	 *            The ask message to display on the standard output prior to reading
-	 *            it from the user.
-	 * @return The read letter.
-	 */
 	private static char readUserInput(Scanner scanner, String askMessage) {
 		System.out.print(askMessage);
-		String word = scanner.nextLine();
-		String upperCaseWord = word.toUpperCase();
-		char firstLetter = upperCaseWord.charAt(0);
+		String word = scanner.nextLine().toUpperCase();
+		char firstLetter = word.charAt(0);
 		return firstLetter;
 	}
 
-	/**
-	 * Prints the specified riddle.
-	 *
-	 * @param solution
-	 *            The solution to print to the standard output.
-	 */
 	private static void printRiddle(char[] solution) {
 		for (char c : solution) {
 			System.out.print(c + " ");
@@ -125,13 +71,6 @@ public class Hangman {
 		System.out.println();
 	}
 
-	/**
-	 * This method generated the masked version of the riddle word.
-	 *
-	 * @param riddleWord
-	 *            The riddle word to generate the masked version for.
-	 * @return The masked version.
-	 */
 	private static char[] generateMaskedSolution(String riddleWord) {
 		char[] mask = new char[riddleWord.length()];
 		for (int i = 0; i < mask.length; i++) {
@@ -144,11 +83,6 @@ public class Hangman {
 		return mask;
 	}
 
-	/**
-	 * This method generates the riddle word that the user will have to guess.
-	 *
-	 * @return The randomly chosen word.
-	 */
 	private static String generateRiddle() {
 		String[] words = { "árvíztűrő tükörfúrógép", "Bőszájú körülíróművész", "Gyümölcsvédő ágyúfűnyíró",
 				"Nyúlfülvágó térközsűrítő", "Túlkábító műrémölőfül", "Zártkörű nőújító ülés", "Üldögélő műújságíró",
